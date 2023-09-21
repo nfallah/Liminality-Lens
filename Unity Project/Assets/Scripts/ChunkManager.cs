4d15bcaceac16add62777a28eb971b820c92264a
Nima Fallah <nfallah399@gmail.com>
2023-09-20 21:12:26 -0400

    Implement basic structure of ChunkManager

--- Unity Project/Assets/Scripts/ChunkManager.cs
+++ Unity Project/Assets/Scripts/ChunkManager.cs
@@ -1,13 +1,37 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ChunkManager : MonoBehaviour
 {
     private static ChunkManager instance;
 
+    public static Dictionary<Vector2Int, Chunk> Chunks { get; private set; }
+
+    public const int chunkSize = 10, renderDistance = 10;
+
+    private Vector2Int playerChunkPos;
+
     private void Awake()
     {
+        if (instance != null)
+        {
+            throw new Exception("Unable to set instance -- value already established.");
+        }
+
         instance = this;
+    }
+
+    private void Update()
+    {
+        Debug.Log(WorldToChunkPos(transform.position));
+    }
+
+    public static Vector2Int WorldToChunkPos(Vector3 worldPos)
+    {
+        return new Vector2Int(
+            (int)(worldPos.x / chunkSize),
+            (int)(worldPos.z / chunkSize));
     }
 
     public static ChunkManager Instance
@@ -20,16 +44,6 @@ public class ChunkManager : MonoBehaviour
             }
 
             return instance;
-        }
-
-        set
-        {
-            if (instance != null)
-            {
-                throw new Exception("Unable to set instance -- value already established.");
-            }
-
-            instance = value;
         }
     }
 }
